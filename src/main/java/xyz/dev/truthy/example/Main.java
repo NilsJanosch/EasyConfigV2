2bcab313451b292cf0e5be3c1c21360d06d30d7c
Uek Lernender 1 <steamnilsoo@gmail.com>
2024-02-29 10:55:02 +0100

    Moved Main.java to the package "example"

---
+++ src/main/java/xyz/dev/truthy/example/Main.java
@@ -0,0 +1,11 @@
+package xyz.dev.truthy.example;
+
+public class Main {
+    /*
+     * This is just an example usage of the config system.
+     * You can delete the whole package "example" if you want :D
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
